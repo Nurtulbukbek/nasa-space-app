4f962a58315280c4891791eda89e49d9bd94c2d9
Resul <owezresul0@gmail.com>
2024-10-05 23:09:58 +0500

    change logog and title

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,10 @@ function HomePage() {
                             <h1>EXPLORE PACE</h1>
                             <h3>
                                 Our project, which is called ExplorePACE, is dedicated to educate young 
-                                generation about PACE mission launched by NASA. 
+                                generation about PACE mission launched by NASA. Our main goal is 
+                                to make science more accessible and enjoyable, therefore we created a website, 
+                                a fun and engaging platform, to spark the student`s interest in the scientific exploration of Earth, 
+                                particularly oceanography and climate change, and help teachers to engage them during the lessons.
                             </h3>
                         </div>
 
